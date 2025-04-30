d0945d3c1ce1e3b368b9dd3b9435f67c8f430df5
Naya Singhania <naya.singhania@gmail.com>
2025-04-29 19:23:13 -0700

    fetch search endpoint from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Search from "@/components/search";
 export default async function Page() {
   return (
     <div className="flex flex-col items-center w-full">
-      <h1>Welcome to Access Finder</h1>
-      <br />
+      <h1 className="mb-4">Welcome to Access Finder</h1>
       <Search size="full" />
     </div>
   );
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
 export default function Page() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+
+  useEffect(() => {
+    if (query) {
+      // Perform the search with the query
+      fetch(`/api/search?query=${query}`)
+        .then((response) => {
+          if (!response.ok) {
+            console.log("Network request failed", response.status);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Search results:", data);
+          // Handle the search results here
+        })
+        .catch(() => {
+          console.error("There was a problem with the fetch operation");
+        });
+    }
+  }, [query]);
+
   return (
     <div>
       <h1>Search</h1>
