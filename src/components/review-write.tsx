9f11d702a91ab7da23fdb87725e6dabac9d545f3
Naya Singhania <naya.singhania@gmail.com>
2025-05-03 16:40:00 -0700

    add rating ui

--- src/components/review-write.tsx
+++ src/components/review-write.tsx
@@ -1,20 +1,40 @@
 "use client";
 
+import { StarIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
+import { useState } from "react";
 
 export default function ReviewWrite({ entity_id }: { entity_id: string }) {
+  const [rating, setRating] = useState(0);
+
+  function stars(rating: number) {
+    return (
+      <div className="flex mb-2 w-24">
+        {[1, 2, 3, 4, 5].map((star) => (
+          <StarIcon
+            key={star}
+            className={`cursor-pointer ${star <= rating ? "text-yellow-500" : ""}`}
+            fill={star <= rating ? "currentColor" : "none"} // Add fill color
+            onClick={() => setRating(star)}
+          />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <form>
       <h2 className="mb-2">Write a Review</h2>
-      <p className="mt-2">[indicators placeholder]</p>
-      <p className="my-2">[rating placeholder]</p>
+      <p className="mt-2">Your rating: {rating} stars</p>
+      {stars(rating)}
+      <p className="mb-2">[indicators placeholder]</p>
       <Textarea
-        disabled
+        disabled={rating === 0}
         className="md:max-w-[50%] mb-2"
         placeholder="Write your review here..."
       ></Textarea>
-      <Button disabled>Submit</Button>
+      <Button disabled={rating === 0}>Submit</Button>
     </form>
   );
 }
