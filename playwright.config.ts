ad20cff5786b3b34b80fab3b119721d7c290b8ad
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2025-05-27 16:24:16 -0700

    add tests

---
+++ playwright.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+	forbidOnly: Boolean(process.env.CI),
+	fullyParallel: true,
+	globalTeardown: "./tests/globalTeardown.ts",
+	outputDir: "playwright/results/",
+	reporter: [
+		["list"],
+		[
+			"monocart-reporter",
+			{
+				name: "Monocart Report",
+				outputFile: "./playwright/results/report.html",
+				coverage: {
+					lcov: true,
+					reports: ["codecov"],
+					entryFilter: (entry: { url: string }) =>
+						(entry.url.includes("next/static/chunks") ||
+							entry.url.includes("next/server/app")) &&
+						!entry.url.includes("node_modules"),
+					sourceFilter: (sourcePath: string) =>
+						sourcePath.includes("src/") && !sourcePath.includes("node_modules"),
+					sourcePath: (fileSource: string) => {
+						const list = ["_N_E/", "playwright-nextjs-coverage-example/"];
+						for (const pre of list) {
+							if (fileSource.startsWith(pre)) {
+								return fileSource.slice(pre.length);
+							}
+						}
+						return fileSource;
+					},
+				},
+			},
+		],
+	],
+	retries: process.env.CI ? 2 : 0,
+	projects: [{ name: "chromium", use: { ...devices["Desktop Chrome"] } }],
+	testDir: "./tests",
+	use: { baseURL: "http://127.0.0.1:3000" },
+	workers: process.env.CI ? 1 : undefined,
+});
---
+++ tests/home.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from "@playwright/test";
+import { addCoverageReport } from "monocart-reporter";
+
+test.beforeEach(async ({ page }) => {
+	await page.coverage.startJSCoverage({ resetOnNavigation: false });
+});
+
+test.afterEach(async ({ page }) => {
+	const coverage = await page.coverage.stopJSCoverage();
+	await addCoverageReport(coverage, test.info());
+});
+
+test("page has title", async ({ page }) => {
+	await page.goto("/");
+	await expect(page.title()).resolves.toBe("Access Finder");
+});
