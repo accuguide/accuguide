d37830a73918f358add082e55ea6ca630dc1a481
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2025-05-27 16:30:27 -0700

    hopefully fix workflow

--- tests/globalTeardown.ts
+++ tests/globalTeardown.ts
@@ -9,58 +9,58 @@ import { fileURLToPath } from "url";
  * Playwright test global teardown to collect server coverage.
  */
 export default async function globalTeardown(config: FullConfig) {
-	const client = await CDP({ port: 9230 });
+  const client = await CDP({ port: 9230 });
 
-	await client.Runtime.enable();
+  await client.Runtime.enable();
 
-	const data = await client.Runtime.evaluate({
-		expression:
-			'new Promise(resolve=>{ require("v8").takeCoverage(); resolve(process.env.NODE_V8_COVERAGE); })',
-		includeCommandLineAPI: true,
-		returnByValue: true,
-		awaitPromise: true,
-	});
+  const data = await client.Runtime.evaluate({
+    expression:
+      'new Promise(resolve=>{ require("v8").takeCoverage(); resolve(process.env.NODE_V8_COVERAGE); })',
+    includeCommandLineAPI: true,
+    returnByValue: true,
+    awaitPromise: true,
+  });
 
-	await client.Runtime.disable();
-	await client.close();
+  await client.Runtime.disable();
+  await client.close();
 
-	const dir = data.result.value;
+  const dir = data.result.value;
 
-	if (!fs.existsSync(dir)) {
-		return;
-	}
+  if (!fs.existsSync(dir)) {
+    return;
+  }
 
-	const files = fs.readdirSync(dir);
-	for (const filename of files) {
-		const content = fs
-			.readFileSync(path.resolve(dir, filename))
-			.toString("utf-8");
-		const json = JSON.parse(content);
-		let coverageList = json.result;
+  const files = fs.readdirSync(dir);
+  for (const filename of files) {
+    const content = fs
+      .readFileSync(path.resolve(dir, filename))
+      .toString("utf-8");
+    const json = JSON.parse(content);
+    let coverageList = json.result;
 
-		coverageList = coverageList.filter((entry: { url: string }) =>
-			entry.url?.startsWith("file:"),
-		);
+    coverageList = coverageList.filter((entry: { url: string }) =>
+      entry.url?.startsWith("file:"),
+    );
 
-		coverageList = coverageList.filter((entry: { url: string }) =>
-			entry.url.includes("next/server/app"),
-		);
+    coverageList = coverageList.filter((entry: { url: string }) =>
+      entry.url.includes("next/server/app"),
+    );
 
-		coverageList = coverageList.filter(
-			(entry: { url: string }) => !entry.url.includes("manifest.js"),
-		);
+    coverageList = coverageList.filter(
+      (entry: { url: string }) => !entry.url.includes("manifest.js"),
+    );
 
-		if (!coverageList.length) {
-			continue;
-		}
+    if (!coverageList.length) {
+      continue;
+    }
 
-		for (const entry of coverageList) {
-			const filePath = fileURLToPath(entry.url);
-			if (fs.existsSync(filePath)) {
-				entry.source = fs.readFileSync(filePath).toString("utf8");
-			}
-		}
+    for (const entry of coverageList) {
+      const filePath = fileURLToPath(entry.url);
+      if (fs.existsSync(filePath)) {
+        entry.source = fs.readFileSync(filePath).toString("utf8");
+      }
+    }
 
-		await addCoverageReport(coverageList, { config } as TestInfo);
-	}
+    await addCoverageReport(coverageList, { config } as TestInfo);
+  }
 }
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,15 +2,15 @@ import { expect, test } from "@playwright/test";
 import { addCoverageReport } from "monocart-reporter";
 
 test.beforeEach(async ({ page }) => {
-	await page.coverage.startJSCoverage({ resetOnNavigation: false });
+  await page.coverage.startJSCoverage({ resetOnNavigation: false });
 });
 
 test.afterEach(async ({ page }) => {
-	const coverage = await page.coverage.stopJSCoverage();
-	await addCoverageReport(coverage, test.info());
+  const coverage = await page.coverage.stopJSCoverage();
+  await addCoverageReport(coverage, test.info());
 });
 
 test("page has title", async ({ page }) => {
-	await page.goto("/");
-	await expect(page.title()).resolves.toBe("Access Finder");
+  await page.goto("/");
+  await expect(page.title()).resolves.toBe("Access Finder");
 });
