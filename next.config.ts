399a9a722fb29143e7346f0b01287a5efb2dc3d7
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2025-05-27 16:46:50 -0700

    fix error

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["mdx", "ts", "tsx"],
-  webpack: (config) => {
-    Object.defineProperty(config, "devtool", {
-      get() {
-        return "source-map";
-      },
-      set() {},
-    });
-    // config.devtool = "source-map";
-    return config;
-  },
 };
 
 const withMDX = createMDX({
