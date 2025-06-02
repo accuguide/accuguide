23c93fdfe305898c3da1d632fd7804daaa2b5fb3
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2025-06-01 19:19:58 -0700

    update example env

--- src/s3/index.ts
+++ src/s3/index.ts
@@ -9,7 +9,7 @@ export const s3Client = new S3Client({
     accessKeyId: process.env.AWS_ACCESS_ID!, // Default LocalStack credentials
     secretAccessKey: process.env.AWS_ACCESS_SECRET!,
   },
-  region: process.env.AWS_REGION!,
+  region: process.env.AWS_S3_REGION!,
 });
 
 // Bucket name
