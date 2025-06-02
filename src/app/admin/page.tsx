2a486667dd71dc57d63bb8dc61bcd3bc791ef1db
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2025-06-02 16:57:14 -0700

    remove error

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { main } from "@/db/update-indicators";
-
-async function updateIndicators() {
-  "use server";
-  await main();
-}
-
 export default function Page() {
   return (
     <div>
       <h1 className="mb-2">Admin Dashboard</h1>
-      <form action={updateIndicators}>
-        <Button type="submit">Update Indicators</Button>
-      </form>
     </div>
   );
 }
