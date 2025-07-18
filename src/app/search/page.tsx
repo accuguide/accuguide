0e1e9e7e7422e0d3914b3a0eb1e5423738b85534
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2025-07-18 11:00:58 -0700

    add ui skeleton (closes #70)

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,17 +5,20 @@ import { useSearchParams } from "next/navigation";
 import Cookies from "js-cookie";
 import { SearchDisplayProps } from "@/lib/types";
 import SearchDisplay from "@/components/search/search-display";
+import SearchSkeleton from "@/components/skeletons/search-skeleton";
 
 function SearchResults() {
   const [googleResponse, setGoogleResponse] = useState<SearchDisplayProps[]>(
     [],
   );
   const [dbResponse, setDbResponse] = useState<SearchDisplayProps[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
   const searchParams = useSearchParams();
   const query = searchParams.get("query");
 
   useEffect(() => {
     if (query) {
+      setIsLoading(true);
       // Get latitude and longitude from cookies
       const latitude = Cookies.get("latitude");
       const longitude = Cookies.get("longitude");
@@ -32,43 +35,53 @@ function SearchResults() {
         .then((data) => {
           setGoogleResponse(data[1].data);
           setDbResponse(data[0].data);
+          console.log(data);
         })
         .catch(() => {
           console.error("There was a problem with the fetch operation");
+        })
+        .finally(() => {
+          setIsLoading(false);
         });
     }
   }, [query]);
 
   return (
     <div>
-      <h2 className="mt-2 my-4">Results</h2>
+      {isLoading ? (
+        <SearchSkeleton />
+      ) : (
+        <>
+          <h2 className="mt-2 my-4">Results</h2>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3">
-        {dbResponse.map((place) => (
-          <SearchDisplay
-            displayType="db"
-            key={place.googleId}
-            id={place.id}
-            googleId={place.googleId}
-            name={place.name}
-            type={place.type}
-            address={place.address}
-          />
-        ))}
-      </div>
-      <h2 className="mt-2 my-4">All Google Results</h2>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3">
-        {googleResponse.map((place) => (
-          <SearchDisplay
-            displayType="google"
-            key={place.googleId}
-            googleId={place.googleId}
-            name={place.name}
-            type={place.type}
-            address={place.address}
-          />
-        ))}
-      </div>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3">
+            {dbResponse.map((place) => (
+              <SearchDisplay
+                displayType="db"
+                key={place.googleId}
+                id={place.id}
+                googleId={place.googleId}
+                name={place.name}
+                type={place.type}
+                address={place.address}
+              />
+            ))}
+          </div>
+          <h2 className="mt-2 my-4">All Google Results</h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3">
+            {googleResponse.map((place) => (
+              <SearchDisplay
+                displayType="google"
+                key={place.googleId}
+                googleId={place.googleId}
+                name={place.name}
+                type={place.type}
+                address={place.address}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
