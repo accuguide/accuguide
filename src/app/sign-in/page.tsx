2f0541bf636bfff1367bd9f8e8f497102b44fbc4
paulclrt <p4ul.claret@gmail.com>
2025-07-23 16:05:02 +0200

    fix(UX): Adding error toast message on failed singin (fixes: #86)

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,5 +1,11 @@
 import SigninForm from "@/components/forms/signin-form";
+import { Toaster } from "@/components/ui/sonner"
 
 export default function Page() {
-  return <SigninForm />;
+  return (
+    <>
+      <Toaster position="top-center" richColors/>
+      <SigninForm />
+    </>
+  );
 }
