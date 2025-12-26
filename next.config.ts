17d2c286e4a2cbd622257d36cf4f72390778604c
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2025-12-25 17:12:39 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
-import createMDX from '@next/mdx'
-import type { NextConfig } from 'next'
+import createMDX from "@next/mdx";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['mdx', 'ts', 'tsx'],
-  trailingSlash: true,
+  pageExtensions: ["mdx", "ts", "tsx"],
+  trailingSlash: false,
   experimental: {
     optimizeCss: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
-})
+});
 
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
