1363f9b3db3f21c0261b6466ecce8d69aa605ab3
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-01-31 16:50:31 -0800

    display public profile with reviews

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-  return <div></div>
+import { eq } from 'drizzle-orm'
+import ReviewDisplay from '@/components/reviews/review-display'
+import { db } from '@/lib/db'
+import { reviewTable } from '@/lib/db/schema'
+
+export default async function ({
+  params,
+}: Readonly<{
+  children: React.ReactNode
+  params: Promise<{ id: string }>
+}>) {
+  const { id } = await params
+  const reviews = await db
+    .select()
+    .from(reviewTable)
+    .where(eq(reviewTable.userId, id))
+
+  return (
+    <div className="max-w-2xl md:col-span-2">
+      <ReviewDisplay
+        write={false}
+        entity_id="profile"
+        entity_type="user"
+        reviews={reviews}
+        indicators={[]}
+      />
+    </div>
+  )
 }
