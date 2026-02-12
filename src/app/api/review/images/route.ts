757d51de3d70b121b0067b2bcb3b47b83dab0349
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-02-11 18:39:06 -0800

    format

--- src/app/api/review/images/route.ts
+++ src/app/api/review/images/route.ts
@@ -1,14 +1,14 @@
-import { db } from "@/lib/db";
-import { reviewImagesTable } from "@/lib/db/schema";
-import { eq } from "drizzle-orm";
-import { NextRequest, NextResponse } from "next/server";
+import { db } from '@/lib/db'
+import { reviewImagesTable } from '@/lib/db/schema'
+import { eq } from 'drizzle-orm'
+import { NextRequest, NextResponse } from 'next/server'
 
 export default function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const reviewId = searchParams.get("reviewId") || "";
+  const searchParams = request.nextUrl.searchParams
+  const reviewId = searchParams.get('reviewId') || ''
 
   return db
     .select()
     .from(reviewImagesTable)
-    .where(eq(reviewImagesTable.reviewId, reviewId));
+    .where(eq(reviewImagesTable.reviewId, reviewId))
 }
