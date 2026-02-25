005f1362dea2e29689df8198dfbd5f8baab90381
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-02-25 20:24:54 +0000

    Update next.config.ts
    Signed-off-by: Naya Singhania <74111380+nayasinghania@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // swcMinify: true,
-  experimental: {
-    // Optional: enable if using SWC fully
-    forceSwcTransforms: true,
-  },
-  // Tell SWC/Babel to output modern JS
-  compiler: {
-    styledComponents: true, // if you use it
-  },
-  // Important: modern browserslist
-  // Remove or comment out any old targets
-  // If you have browserslist in package.json, set:
-  // "browserslist": [">0.25%", "not dead"]
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  /* config options here */
 }
 
 export default nextConfig
