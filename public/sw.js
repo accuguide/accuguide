3ba9bcd0a0cb21f3a724ffd9604982de4901a548
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-03-02 00:57:36 +0000

    Refactor service worker by removing caching logic
    Removed static asset caching logic and added optional skipWaiting and clients.claim calls.
    
    Signed-off-by: Naya Singhania <74111380+nayasinghania@users.noreply.github.com>

--- public/sw.js
+++ public/sw.js
@@ -1,49 +1,7 @@
-const CACHE_NAME = 'accuguide-v1'
-
-const STATIC_ASSETS = [
-  '/',
-  '/search',
-  '/info/about',
-  '/info/donate',
-  '/info/volunteer',
-  '/help/faq',
-  '/help/resources',
-]
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(STATIC_ASSETS)
-    }),
-  )
-  self.skipWaiting()
+self.addEventListener('install', () => {
+  // optional: self.skipWaiting()
 })
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name)),
-      )
-    }),
-  )
-  self.clients.claim()
-})
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const responseClone = response.clone()
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseClone)
-        })
-        return response
-      })
-      .catch(() => {
-        return caches.match(event.request)
-      }),
-  )
+self.addEventListener('activate', () => {
+  // optional: clients.claim()
 })
