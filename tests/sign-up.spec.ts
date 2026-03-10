8425f9f13a7b802cb09a7a42d54827cc2888e97c
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-03-10 15:44:13 -0700

    clean up workflow files

--- tests/sign-up.spec.ts
+++ tests/sign-up.spec.ts
@@ -2,7 +2,9 @@ import { test, expect } from '@playwright/test'
 
 test('test', async ({ page }) => {
   await page.goto('/sign-up')
-  await expect(page).toHaveTitle('Sign Up | Accuguide - Discover accessibility')
+  await expect(page).toHaveTitle(
+    'Sign Up |   Accuguide - Discover accessibility',
+  )
   await page.locator('#email').fill('test@playwright.dev')
   await page.locator('#username').fill('playwright')
   await page.locator('#password').fill('Password123!')
