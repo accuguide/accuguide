8540a8e92b8a67cfc10b80b621abd15d9a5912a1
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-03-10 15:19:52 -0700

    check sign up title

--- tests/sign-up.spec.ts
+++ tests/sign-up.spec.ts
@@ -1,7 +1,8 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test('test', async ({ page }) => {
   await page.goto('/sign-up')
+  await expect(page).toHaveTitle('Sign Up | Accuguide - Discover accessibility')
   await page.locator('#email').fill('test@playwright.dev')
   await page.locator('#username').fill('playwright')
   await page.locator('#password').fill('Password123!')
